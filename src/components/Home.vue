43671d6de000575ccf9a2de678f223ca3994eb1e
raya <raya@rhp-valuation.com>
2021-12-22 14:45:29 +0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div>
+      <h1 class="font-bold text-5xl">
+        <VueTextGlitch fill="#F0F0F0" text="Raya Sabari" background></VueTextGlitch>
+      </h1>
+      <div>
+        <vue-typer
+          class="font-medium"
+          :text="['Full Stack Developer','Wordpress Developer']"
+          :repeat="Infinity"
+          :shuffle="false"
+          initial-action="typing"
+          :pre-type-delay="70"
+          :type-delay="70"
+          :pre-erase-delay="2000"
+          :erase-delay="70"
+          erase-style="backspace"
+          :erase-on-complete="false"
+          caret-animation="smooth"
+        ></vue-typer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueTextGlitch from "vue-text-glitch";
+export default {
+  components: {
+    VueTextGlitch,
+  },
+};
+</script>
+
+<style>
+.vue-typer .custom.char {
+  color: #f0f0f0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './index.css'
+import VueTyperPlugin from 'vue-typer'
+Vue.use(VueTyperPlugin)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
