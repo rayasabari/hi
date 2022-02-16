5881662b1983e2edbb985fa338f3365c980f8db8
raya <rayavexed@gmail.com>
2022-02-16 19:44:42 +0700

    wip

--- src/components/partials/Highlight.vue
+++ src/components/partials/Highlight.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="inline-flex px-3 py-0 mx-2 text-gray-700 transition duration-500 transform bg-white rounded-full shadow-md dark:text-gray-300 dark:bg-gray-800 xl:py-1 xl:px-4 text-md xl:text-xl dark:hover:bg-gray-700 focus:outline-none hover:-translate-y-1 hover:shadow-xl"
+    class="inline-flex px-3 py-0 mx-2 font-medium text-gray-700 transition duration-500 transform bg-white rounded-full shadow-md dark:text-gray-300 dark:bg-gray-800 xl:py-1 xl:px-4 text-md xl:text-xl dark:hover:bg-gray-700 focus:outline-none hover:-translate-y-1 hover:shadow-xl"
   >
     <slot></slot>
   </div>
