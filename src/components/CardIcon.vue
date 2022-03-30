82a029803718258b73f3a42a7ad87fd1144f7cb2
raya <rayavexed@gmail.com>
2022-03-30 11:54:11 +0700

    wip

--- src/components/CardIcon.vue
+++ src/components/CardIcon.vue
@@ -11,15 +11,15 @@
       @mouseleave="itemName = ''"
       class="flex flex-col items-center justify-center m-5 transition duration-300 opacity-70 dark:opacity-60 lg:m-7 saturate-0 dark:brightness-150 brightness-100 hover:brightness-100 dark:hover:brightness-100 hover:saturate-100 hover:opacity-100 dark:hover:opacity-100 dark:hover:contrast-150 hover:contrast-150 filter hover:scale-125"
     >
-      <img
-        data-aos="fade-up"
-        data-aos-duration="500"
-        :data-aos-delay="600 + (index * 150)"
-        :src="`./images/${item.icon}`"
-        :alt="item.name"
-        :class="item.icon == 'bootstrap.svg' ? 'w-auto' :'w-6 lg:w-8 xl:w-10'"
-        class="h-6 lg:h-8 xl:h-10"
-      />
+      <div class="flex items-center justify-center w-6 h-6 lg:h-8 xl:h-10 lg:w-8 xl:w-10">
+        <img
+          data-aos="fade-up"
+          data-aos-duration="500"
+          :data-aos-delay="600 + (index * 150)"
+          :src="`./images/${item.icon}`"
+          :alt="item.name"
+        />
+      </div>
       <div
         v-if="itemName == item.name"
         class="absolute mt-16 text-center text-gray-700 transition duration-300 dark:text-gray-300 text-3xs lg:text-2xs font-extralight lg:mt-20"
